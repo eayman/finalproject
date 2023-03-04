cab688f1a717c254450941f810e1a0a6c54983f3
eayman <amawieyad@gmail.com>
2023-03-04 20:18:04 +0200

    edit forms

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,7 @@
 from django.forms import ModelForm
 from .models import *
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 
 class LeadModelForm(ModelForm):
@@ -15,7 +16,36 @@ class LeadModelForm(ModelForm):
     def __init__(self, *args, **kwargs ):
         super(LeadModelForm,self).__init__(*args, **kwargs)
         
-        for name, field in self.fields.items():
+        for label, field in self.fields.items():
             field.widget.attrs.update({'class':'mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"',})
         
-        #self.fields['title'].widget.attrs.update({'class':'input','placeholder':'Add Project Title here ..'})
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2'
+        )
+        def save(self, commit = True):
+            user = super(CustomUserCreationForm, self).save(commit= False)
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.email = self.cleaned_data['email']
+
+            if commit:
+                user.save()
+        
+    def __init__(self, *args, **kwargs ):
+            super(CustomUserCreationForm,self).__init__(*args, **kwargs)
+        
+            for label, field in self.fields.items():
+                field.widget.attrs.update({'class':'mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"',})
+        
+        
+            
+        
--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('',LeadListView.as_view(),name='lead-list'),
     path('lead-create',LeadCreateView.as_view(),name='lead-create'),
     path('<int:pk>/update/',LeadUpdateView.as_view(), name='lead-update'),
-    path('<int:pk>/delete/',LeadDeleteView.as_view(), name='lead-delete'),
+    path('<int:pk>/lead-delete/',LeadDeleteView.as_view(), name='lead-delete'),
     path('agents',AgentListView.as_view(),name='agent-list'),
+    path('agent-create',AgentCreateView.as_view(),name='agent-create'),
+    path('<int:pk>/agent-delete/',AgentDeleteView.as_view(), name='agent-delete'),
 
 
 ]
