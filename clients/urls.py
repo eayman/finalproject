f720cc2b9efb4663effb60b71c0ed6851c00f24c
eayman <amawieyad@gmail.com>
2023-03-06 22:52:05 +0200

    edit urls names

--- clients/urls.py
+++ clients/urls.py
@@ -3,10 +3,9 @@ from clients.views import *
 
 app_name = "clients"
 urlpatterns = [
-    path ('clients/client_list', Client_list.as_view(),name='client_list'),
-    path('clients/<int:pk>/client_delete/', Delete_client.as_view(), name='client_delete') ,
-    path('clients/client_create',ClientCreateView.as_view(),name='client_create'),
-    path('clients/<int:pk>/client_update/',ClientUpdateView.as_view(), name='client_update'),
-     
+    path ('clients/client-list', Client_list.as_view(),name='client-list'),
+    path('clients/client-create',ClientCreateView.as_view(),name='client-create'),
+    path('clients/<int:pk>/client_update/',ClientUpdateView.as_view(), name='client-update'),
+    path('clients/<int:pk>/client-delete/', Delete_client.as_view(), name='client-delete') ,
      
 ]
