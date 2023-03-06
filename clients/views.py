a86120ff1c82a541c309fb58d1bb84b01b7d6d9f
shams-od <shamsodod@gmail.com>
2023-03-06 16:21:20 +0200

    add class (client_list) to view

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import *
 from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView 
 
 
 class OffersPageView(TemplateView):
     template_name = "offers.html"
+    
+class Client_list(TemplateView):
+    template_name ="clients/client_list.html/"
+    queryset = Client.objects.all()
+    context_object_name = "leads"
+
+    def get_queryset(self):
+            return Client.objects.all()
+        
