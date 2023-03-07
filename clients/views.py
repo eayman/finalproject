85c314103f3955d1f2d467c5963f7b5cfd914078
shams-od <shamsodod@gmail.com>
2023-03-07 12:12:55 +0200

    modify to client_list

--- clients/views.py
+++ clients/views.py
@@ -8,24 +8,24 @@ class OffersPageView(TemplateView):
     template_name = "offers.html"
     
 class Client_list(ListView):
-    template_name ="clients/client_list.html"
+    template_name ="clients/client-list.html"
     queryset = Client.objects.all()
     context_object_name = "client"
     
 
 class Delete_client(DeleteView):
-    template_name = "clients/client_delete.html"
+    template_name = "clients/client-delete.html"
     queryset = Client.objects.all()
     
     def get_success_url(self):
-        return resolve_url("clients:client_list")
+        return resolve_url("clients:client-list")
     
 class ClientCreateView(CreateView):
-    template_name = "clients/client_create.html"
+    template_name = "clients/client-create.html"
     form_class = clientModelForm
     
     def get_success_url(self):
-        return resolve_url("clients:client_list")
+        return resolve_url("clients:client-list")
     
 class ClientUpdateView(UpdateView):
     template_name = "clients/clients_update.html"
@@ -33,13 +33,13 @@ class ClientUpdateView(UpdateView):
     form_class = clientModelForm
     
     def get_success_url(self):
-        return resolve_url("clients:client_list")
+        return resolve_url("clients:client-list")
     
 
 class ClientDeleteView(DeleteView):
-    template_name = "clients/client_delete.html"
+    template_name = "clients/client-delete.html"
     queryset = Client.objects.all()
     
     def get_success_url(self):
-        return resolve_url("clients:client_list")
+        return resolve_url("clients:client-list")
     
