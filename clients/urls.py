a7afdeb67f62e990a9e61e56013efcee000cbb0e
eayman <amawieyad@gmail.com>
2023-03-07 21:56:46 +0200

    edit urls files

--- clients/urls.py
+++ clients/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('clients/client-list', ClientListView.as_view(),name='client-list'),
     path('clients/client-create',ClientCreateView.as_view(),name='client-create'),
     path('clients/<int:pk>/client-update/',ClientUpdateView.as_view(), name='client-update'),
-    path('clients/<int:pk>/client-delete/', ClienDeletetView.as_view(), name='client-delete') ,
-     
+    path('clients/<int:pk>/client-delete/', ClienDeletetView.as_view(), name='client-delete'),
 ]
