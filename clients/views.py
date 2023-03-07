3b82cd3fb002141ce2f2e4f0fbde12702d9853b8
shams-od <shamsodod@gmail.com>
2023-03-07 13:25:40 +0200

    edit forms

--- clients/views.py
+++ clients/views.py
@@ -6,30 +6,13 @@ from django.views.generic import TemplateView,ListView,CreateView,UpdateView,Del
 
 class OffersPageView(TemplateView):
     template_name = "offers.html"
-    
-<<<<<<< HEAD
-class Client_list(ListView):
-    template_name ="clients/client-list.html"
-    queryset = Client.objects.all()
-    context_object_name = "client"
-    
-
-class Delete_client(DeleteView):
-    template_name = "clients/client-delete.html"
-    queryset = Client.objects.all()
-    
-    def get_success_url(self):
-        return resolve_url("clients:client-list")
-    
-=======
 class ClientListView(ListView):
     template_name ="clients/client_list.html"
     queryset = Client.objects.all()
     context_object_name = "client"
     
->>>>>>> 5697b6d3dcf7756ab597ef6a5f95f5acd227254d
 class ClientCreateView(CreateView):
-    template_name = "clients/client-create.html"
+    template_name = "clients/client_create.html"
     form_class = clientModelForm
     
     def get_success_url(self):
@@ -44,12 +27,8 @@ class ClientUpdateView(UpdateView):
         return resolve_url("clients:client-list")
     
 
-class ClientDeleteView(DeleteView):
-    template_name = "clients/client-delete.html"
 class ClienDeletetView(DeleteView):
     template_name = "clients/client_delete.html"
     queryset = Client.objects.all()
-    
     def get_success_url(self):
         return resolve_url("clients:client-list")
-    
